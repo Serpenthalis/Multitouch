2c4ab6c71a06fa3758f4605dc1d66bce05a2ef85
Joshua Kirschner <joshua.kirschner@hotmail.de>
2020-01-18 20:24:36 +0100

    Simple client implementation

---
+++ Client/mt_framework_2019_cv.4.11/EventListener.cpp
@@ -0,0 +1,37 @@
+#include "EventListener.h"
+
+EventListener::EventListener()
+{
+}
+
+EventListener::~EventListener()
+{
+}
+
+void EventListener::addTuioCursor(TuioCursor* tcur)
+{
+}
+
+void EventListener::updateTuioCursor(TuioCursor* tcur)
+{
+}
+
+void EventListener::removeTuioCursor(TuioCursor* tcur)
+{
+}
+
+void EventListener::addTuioObject(TuioObject* tobj)
+{
+}
+
+void EventListener::updateTuioObject(TuioObject* tobj)
+{
+}
+
+void EventListener::removeTuioObject(TuioObject* tobj)
+{
+}
+
+void EventListener::refresh(TuioTime ftime)
+{
+}
---
+++ Client/mt_framework_2019_cv.4.11/EventListener.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "TuioServer.h"
+#include "TuioTime.h"
+#include "TuioCursor.h"
+#include <TuioListener.h>
+#include "TuioClient.h"
+#include <cmath>
+#include <map>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+using namespace cv;
+using namespace TUIO;
+
+class EventListener : public TuioListener
+{
+private:
+	int testy;
+
+public:
+	EventListener();
+	~EventListener();
+	void addTuioCursor(TuioCursor* tcur);
+	void updateTuioCursor(TuioCursor* tcur);
+	void removeTuioCursor(TuioCursor* tcur);
+	void addTuioObject(TuioObject* tobj);
+	void updateTuioObject(TuioObject* tobj);
+	void removeTuioObject(TuioObject* tobj);
+	void refresh(TuioTime ftime);
+};
+
